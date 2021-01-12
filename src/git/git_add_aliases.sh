c2e0b85d1d42e7874ea582e0ad8c24bbd69254fb
Martin M <martin.masson@datastorm.fr>
2021-01-12 16:23:00 +0100

    feat : add more git aliases

--- src/git/git_add_aliases.sh
+++ src/git/git_add_aliases.sh
@@ -1,1 +1,4 @@
+git config --global alias.co checkout
+git config --global alias.push-f "push --force-with-lease"
 git config --global alias.slog "log --oneline"
+git config --global alias.undo "reset --soft HEAD^"
