fc73bd63e64584f2c2109c0ba56dc3e9871c6671
Martin M <martin.masson@datastorm.fr>
2021-01-14 08:33:02 +0100

    feat : add git alias untrack

--- src/git/git_add_aliases.sh
+++ src/git/git_add_aliases.sh
@@ -1,4 +1,5 @@
 git config --global alias.co checkout
+git config --global alias.untrack "rm --cached"
 git config --global alias.push-f "push --force-with-lease"
 git config --global alias.slog "log --oneline"
 git config --global alias.undo "reset --soft HEAD^"
