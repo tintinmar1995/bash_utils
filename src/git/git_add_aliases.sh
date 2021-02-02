4bdbe048e0b25012a8c749ef6808870d89fc4a9f
Martin <martin.masson@datastorm.fr>
2021-02-02 15:39:20 +0000

    add git ignore/wip and activate autocorrect

--- src/git/git_add_aliases.sh
+++ src/git/git_add_aliases.sh
@@ -1,5 +1,9 @@
+git config --global help.autocorrect 20
+
 git config --global alias.co checkout
 git config --global alias.untrack "rm --cached"
 git config --global alias.push-f "push --force-with-lease"
 git config --global alias.slog "log --oneline"
 git config --global alias.undo "reset --soft HEAD^"
+git config --global alias.wip '!git add -A && git commit -m "Temp"'
+git config --global alias.ignore "!f() { echo $1 >> .gitignore; }; f"
