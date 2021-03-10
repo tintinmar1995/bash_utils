aade3be3fd332f874dead2fc0ceeb1b61f001e9d
Martin <martin.masson@protonmail.com>
2021-03-10 18:21:15 +0100

    Update setup.sh

--- src/workspace/setup.sh
+++ src/workspace/setup.sh
@@ -7,7 +7,7 @@ git config --global commit.template $commit_template_ds
 ssh-keygen
 
 echo CONFIG PYTHON..
-echo --> https://github.com/pyenv/pyenv
-echo --> https://github.com/pyenv/pyenv-virtualenv
-echo --> pyenv install 3.9.0
-echo --> pyenv virtualenv 3.9.0 BLABLA
+echo --> 'https://github.com/pyenv/pyenv'
+echo --> 'https://github.com/pyenv/pyenv-virtualenv'
+echo --> 'pyenv install 3.9.0'
+echo --> 'pyenv virtualenv 3.9.0 BLABLA'
