0d3ce02cfde8626ada8517d8b9896e4ea38b2dc6
tintinmar1995 <martin.masson@protonmail.com>
2021-03-19 11:43:35 +0100

    Add a few aliases and fix ignore

--- src/git/git_add_aliases.sh
+++ src/git/git_add_aliases.sh
@@ -1,9 +1,11 @@
 git config --global help.autocorrect 20
 
+git config --global alias.aliases "!git config --get-regexp '^alias\.'"
 git config --global alias.co checkout
-git config --global alias.untrack "rm --cached"
+git config --global alias.br branch
+git config --global alias.st status
 git config --global alias.push-f "push --force-with-lease"
 git config --global alias.slog "log --oneline"
-git config --global alias.undo "reset --soft HEAD^"
-git config --global alias.wip '!git add -A && git commit -m "Temp"'
-git config --global alias.add-last '!git add $(!:2)'
+git config --global alias.wip '!git add -A && git commit -m "Wip"'
+git config --global alias.ignore "! echo $1 >> .gitignore"
+git config --global alias.ignored '! git ls-files --others --ignored --exclude-standard --directory && git ls-files --others -i --exclude-standard'
