c4a3a58c8d08c5b85cf2e9dbe7afd4c8129a0438
Martin MASSON <martin.masson@datastorm.fr>
2021-08-27 16:00:07 +0000

    No pre-commit in git wip

--- src/git/git_add_aliases.sh
+++ src/git/git_add_aliases.sh
@@ -10,7 +10,7 @@ git config --global alias.br branch
 git config --global alias.st status
 git config --global alias.push-f "push --force-with-lease"
 git config --global alias.slog "log --oneline"
-git config --global alias.wip '!git add -A && git commit -m "Wip"'
+git config --global alias.wip '!git add -A && git commit -m "WIP : To be erased soon" --no-verify'
 git config --global alias.ignore "! echo $1 >> .gitignore"
 git config --global alias.ignored '! git ls-files --others --ignored --exclude-standard --directory && git ls-files --others -i --exclude-standard'
 git config --global alias.unfollowed 'update-index --assume-unchanged'
