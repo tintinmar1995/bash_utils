8c89acd66c4d0dcf0eebd4720744725b756e0aed
Martin MASSON <martin.masson@datastorm.fr>
2021-08-31 14:44:17 +0000

    Less history in git slog

--- src/git/git_add_aliases.sh
+++ src/git/git_add_aliases.sh
@@ -9,7 +9,7 @@ git config --global alias.camend "commit --amend --no-edit"
 git config --global alias.br branch
 git config --global alias.st status
 git config --global alias.push-f "push --force-with-lease"
-git config --global alias.slog "log --oneline"
+git config --global alias.slog "log --oneline -n 10"
 git config --global alias.wip '!git add -A && git commit -m "WIP : To be erased soon" --no-verify'
 git config --global alias.ignore "! echo $1 >> .gitignore"
 git config --global alias.ignored '! git ls-files --others --ignored --exclude-standard --directory && git ls-files --others -i --exclude-standard'
