88f085976b21ca1b069d0caa3996e4ef0015743d
Martin Masson <martin.masson@datastorm.fr>
2023-03-24 16:07:47 +0100

    ENH: Add comment in install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+# WARNING: To be sourced (source install.sh / . install.sh)
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo source $DIR/bash_aliases >> ~/.bash_aliases
 echo source $DIR/bashrc >> ~/.bashrc
